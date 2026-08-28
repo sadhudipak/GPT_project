d0d10212078bcd7983b645aa6b25b5fab1755916
Dipak Sadhu <sadhudipak892@gmail.com>
2026-08-28 08:53:17 +0530

    done

--- Frontend/src/utils/api.js
+++ Frontend/src/utils/api.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = import.meta.env.VITE_API_URL;
+export const API_BASE_URL = import.meta.env.VITE_API_URL||"http://localhost:5173";
 
 // Wrapper around fetch that:
 // - prefixes the backend base URL
